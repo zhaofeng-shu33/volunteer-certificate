3da99d1dcf40913a4f7c3556dc4cd1e2408016f3
zhaofeng-shu33 <616545598@qq.com>
2020-03-06 23:05:22 +0800

    add handle event

--- frontend/admin.js
+++ frontend/admin.js
@@ -5,20 +5,28 @@ export default class Admin extends React.Component {
     super(props);
     this.state = {
       token: "",
+      file: null
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleImageSelect = this.handleImageSelect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e) {
     let new_json = {}
     new_json[e.target.name] = e.target.value;
     this.setState( new_json );
   }
-
+  handleImageSelect(e) {
+    this.setState({file: e.target.file});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
     <div>
       Admin page
-      <form>
+      <form onSubmit={this.handleSubmit}>
       <div class="input-group mb-3">
       <div class="input-group-prepend"><span class="input-group-text">
         Token</span></div>
@@ -28,7 +36,8 @@ export default class Admin extends React.Component {
       <div class="form-group">
         <label for="template">Template Image</label>
         <input type="file" class="form-control-file"
-               id="template" accept=".jpg"/>
+               id="template" accept=".jpg"
+               onChange={this.handleImageSelect}/>
       </div>
       <div class="form-group">
         <button type="submit" class="btn btn-primary">
